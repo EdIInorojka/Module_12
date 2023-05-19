7a7551a1b161c088dcd165cddbf448234bf2a3e5
Koh Aleksey <alexckokh@gmail.com>
2023-05-19 20:48:31 +0500

    19

--- js/index.js
+++ js/index.js
@@ -86,3 +86,25 @@ questionBtn.forEach(function(element){
         e.currentTarget.classList.add('questions-button--active');
     });
 });
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
